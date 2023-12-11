88f7ad61e970917cafb9f3e0995c27a1d66ebab8
Megumin <megumin_lyq@outlook.com>
2023-12-11 17:44:16 +0800

    继续

--- lib/widget/Constants.dart
+++ lib/widget/Constants.dart
@@ -4,7 +4,7 @@ import 'package:hexcolor/hexcolor.dart';
 class Constants{
   static Color mainColor = HexColor("#41CED7");
   static var titleTextStyle =  TextStyle(color: Constants.mainColor,fontSize: 24, fontWeight: FontWeight.bold);
-
+  static var greyTextStyle = TextStyle(color: HexColor("#9C9C9C"),fontSize: 20,fontWeight: FontWeight.normal);
   static var mainButtonTextStyleB = const TextStyle(color: Colors.white,fontSize: 24, fontWeight: FontWeight.bold);
   static var mainButtonStyleB = ButtonStyle(
     minimumSize: MaterialStateProperty.all(const Size(264, 48)),
--- lib/widget/page/page_LoginByPwd.dart
+++ lib/widget/page/page_LoginByPwd.dart
@@ -1,20 +1,83 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
 import '../Constants.dart';
 
-class LoginByPwd extends StatefulWidget{
+class LoginByPwd extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _LoginByPwdState();
   }
-
 }
-class _LoginByPwdState extends State<LoginByPwd>{
+
+class _LoginByPwdState extends State<LoginByPwd> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Constants.mainColor,
+    PhoneNumber phoneNumber = PhoneNumber(isoCode: "CN");
+    SelectorConfig selectorConfig = SelectorConfig(
+      showFlags: false,
+      setSelectorButtonAsPrefixIcon: true,
+      leadingPadding: 0,
+      trailingSpace: false,
+    );
+    return Material(
+      child: Container(
+        color: Colors.white,
+        child: Container(
+          margin: EdgeInsets.fromLTRB(43, 90, 18, 0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Align(
+                  alignment: Alignment.topRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "验证码登录",
+                      style: Constants.greyTextStyle,
+                    ),
+                  )),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "密码登录",
+                  style: Constants.titleTextStyle.copyWith(fontSize: 30),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 60, 42, 0),
+                child: Column(
+                  children: [
+                    InternationalPhoneNumberInput(
+                      onInputChanged: (number) {},
+                      initialValue: phoneNumber,
+                      spaceBetweenSelectorAndTextField: 1,
+                      selectorConfig: selectorConfig,
+                      hintText: "请输入手机号码",
+                    ),
+                    const Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(hintText: "输入密码"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                  padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+                  child: ElevatedButton(
+                      style: Constants.mainButtonStyleB,
+                      onPressed: () {},
+                      child: Text("登录",style: Constants.mainButtonTextStyleB,)))
+            ],
+          ),
+        ),
+      ),
     );
   }
-
 }
