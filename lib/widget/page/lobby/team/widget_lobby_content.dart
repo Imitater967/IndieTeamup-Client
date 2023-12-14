39837577e66382e1f8b86a5317d96cc661ece566
Megumin <megumin_lyq@outlook.com>
2023-12-14 11:08:36 +0800

    继续继续

--- lib/widget/page/lobby/team/widget_lobby_content.dart
+++ lib/widget/page/lobby/team/widget_lobby_content.dart
@@ -62,10 +62,36 @@ class _LobbyContentItemState extends State<_LobbyContentItem> {
           Row(
             children: [
               Text("已经认证"),
-              Placeholder(),
+              SizedBox(width: 10,),
+              Image.asset("assets/art/avatar/icon_resume_identification_lv0.png",width: 14,height: 14,),
+              Container(margin: EdgeInsets.symmetric(horizontal: 10),color: Colors.black26,child: Text("小微企业"),),
+              Container(color: Colors.black26,child: Text("中国香港"),),
+            ],
+          ),
+          Row(
+            children: [
+              Image.asset("assets/art/avatar/bg_resume_avatar.png",height: 50,width: 50,),
+              Expanded(
+                child: Column(mainAxisAlignment: MainAxisAlignment.center,children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text("林希",textAlign: TextAlign.left,),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text("已投项目"),
+                      SizedBox(width: 10,),
+                      Text("保X萝X"),
+                      SizedBox(width: 10,),
+                      Text("王X荣X"),
+                    ],
+                  )
+                ],),
+              )
             ],
           )
-
         ],
       ),
     );
