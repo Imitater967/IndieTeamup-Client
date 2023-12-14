c6e28098d2e9796b93511985f5e1ceae1cd6750a
Megumin <megumin_lyq@outlook.com>
2023-12-14 09:55:04 +0800

    完成

--- lib/widget/page/lobby/team/page_LobbyTeamOverview.dart
+++ lib/widget/page/lobby/team/page_LobbyTeamOverview.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:indie_team_up/widget/page/lobby/team/widget_lobby_sub_tabbar.dart';
 
-class LobbyTeamOverview extends StatefulWidget{
+class LobbyTeamOverview extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _LobbyTeamOverviewState();
   }
-
 }
 
-class _LobbyTeamOverviewState extends State<LobbyTeamOverview> with SingleTickerProviderStateMixin{
+class _LobbyTeamOverviewState extends State<LobbyTeamOverview>
+    with SingleTickerProviderStateMixin {
   late TabController _parentController;
 
   @override
@@ -17,41 +18,56 @@ class _LobbyTeamOverviewState extends State<LobbyTeamOverview> with SingleTicker
     super.initState();
     _parentController = TabController(length: 3, vsync: this);
   }
+
   @override
   void dispose() {
     super.dispose();
     _parentController.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
       body: Column(
         children: [
           Container(
             height: 45,
             decoration: BoxDecoration(
-              color:  Colors.grey,
+              color: Colors.grey,
               borderRadius: BorderRadius.circular(25),
             ),
             child: TabBar(
               controller: _parentController,
-              indicator: BoxDecoration(borderRadius: BorderRadius.circular(25),color: Colors.green),
+              indicator: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25), color: Colors.green),
               labelColor: Colors.white,
               unselectedLabelColor: Colors.black,
-              tabs: [Tab(text: "风险投资",),Tab(text: "股权投资",),Tab(text: "债券投资",)],
+              tabs: [
+                Tab(
+                  text: "风险投资",
+                ),
+                Tab(
+                  text: "股权投资",
+                ),
+                Tab(
+                  text: "债券投资",
+                )
+              ],
             ),
           ),
-          Expanded(child: TabBarView(
+          Expanded(
+              child: TabBarView(
             controller: _parentController,
             children: [
-              Text("风险投资"),
-              Text("股权投资"),
-              Text("债券投资"),
+              LobbySubTabBar(),
+              LobbySubTabBar(),
+              LobbySubTabBar(),
             ],
           ))
         ],
       ),
     );
   }
-
 }
---
+++ lib/widget/page/lobby/team/widget_lobby_sub_tabbar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LobbySubTabBar extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LobbySubTabBarState();
+  }
+}
+
+class _LobbySubTabBarState extends State<LobbySubTabBar>
+    with SingleTickerProviderStateMixin {
+  late TabController _subController;
+
+  @override
+  void initState() {
+    super.initState();
+    _subController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _subController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [TabBar(
+        controller: _subController,
+        tabs: [
+          Tab(
+            text: "推荐",
+          ),
+          Tab(
+            text: "附近",
+          ),
+          Tab(
+            text: "最新",
+          ),
+        ],
+        indicator: BoxDecoration(
+            borderRadius: BorderRadius.circular(25), color: Colors.green),
+        labelColor: Colors.white,
+        unselectedLabelColor: Colors.black,
+      ),
+      Expanded(child: TabBarView(controller: _subController,children: [Text("data"),Text("data"),Text("data")],)),
+
+      ]
+    );
+  }
+}
